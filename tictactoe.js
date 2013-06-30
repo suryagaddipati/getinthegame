5522c363fe3ee9b24045cd56e284a54a903d5708
beanified <shuling.symc@gmail.com>
2013-06-30 11:23:45 -0500

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
 messages = [ ['http://i.imgur.com/iCHPU5N.jpg', 1],
          ['http://i.imgur.com/Ea52gez.jpg', 2],
-         ['http://i.imgur.com/amMr8I0.jpg', 3],
+         ['http://i.imgur.com/lAU3CBY.jpg', 3],
          [ 'http://i.imgur.com/ms5fFzt.jpg', 4],
          ]
          
